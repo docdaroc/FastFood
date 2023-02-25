cd6b44c4e386eb6d75326ae199e427c748698fb3
Deyan Kazashki <dkazashki@gmail.com>
2023-02-25 16:21:15 +0200

    last update

--- Meal.java
+++ Meal.java
@@ -1,45 +1,51 @@
 package BurgerChallenge;
 
 public class Meal {
-     private String type;
-     private String size;
-     private double price;
+    //private String type;
+    private String mealSize;
 
-     public Meal() {
-          this("Hamburger");
-     }
+    private double price;
 
-     public Meal(String type) {
-          Burger burger = new Burger(type);
-          if (!burger.getType().equals("Hamburger")){
-               Burger.singleBurgerOrder(burger.getType());
-              // Burger.Toppings.withTopping(burger.getType());
-          }
-          System.out.print("\tPlease choose drink kind: ");
-          String drinkKind = Main.scanner.nextLine();
-          Drink drink = new Drink(drinkKind, "");
-          System.out.print("\tPlease choose side item kind: ");
-          String sideItemKind = Main.scanner.nextLine();
-          SideItem sideItem = new SideItem(sideItemKind, "");
-          if (type.toLowerCase().contains("ham")){
-               drink.setSize("small");
-               sideItem.setSize("small");
-               price = 8;
-          }else if (type.toLowerCase().contains("che")){
+    public Meal() {
+        this("Hamburger", "small");
+    }
 
-               price = 12;
-          }else if (type.toLowerCase().contains("cha")){
-               price = 13;
-          }else {
-               price = 10;
-          }
-     }
+    public Meal(String type, String mealSize) {
+        Burger burger = new Burger(type);
+        if (!type.equals("Hamburger")) {
+            Burger.singleBurgerOrder(burger.getType());
+        }
+        System.out.print("\tPlease choose drink kind: ");
+        String drinkKind = Main.scanner.nextLine();
+        Drink drink = new Drink(drinkKind, mealSize);
+        System.out.print("\tPlease choose side item kind: ");
+        String sideItemKind = Main.scanner.nextLine();
+        SideItem sideItem = new SideItem(sideItemKind, mealSize);
+        this.price = priceCalculation(type, mealSize);
 
-     public double getPrice() {
-          return price;
-     }
+    }
 
-     public void setSize(String size) {
-          this.size = size;
-     }
+
+    private double priceCalculation(String type, String mealSize) {
+        if (type.toLowerCase().contains("ham")) {
+            price = 8;
+        } else if (type.toLowerCase().contains("che")) {
+            price = 12;
+        } else if (type.toLowerCase().contains("cha")) {
+            price = 13;
+        }
+
+        if (mealSize.toLowerCase().contains("med")) {
+            price += 3.5;
+        } else if (type.toLowerCase().contains("lar")) {
+            price += 5;
+        }
+
+        return price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
 }

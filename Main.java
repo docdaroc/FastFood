53d306c3c401e9ba8da2b344d8a18b8efef08675
Deyan Kazashki <dkazashki@gmail.com>
2023-02-26 12:01:59 +0200

    last now

--- Main.java
+++ Main.java
@@ -30,11 +30,8 @@ public class Main {
                 case 1 -> {
                     System.out.print("\tPlease choose a burger type - available choices are:" +
                             "\n\t\t- Hamburger\n\t\t- Cheese burger" +
-<<<<<<< HEAD
                             "\n\t\t- Deluxe burger\n\t\t- Charm burger\n\tEnter your choice [by kind of burger]: ");
-=======
-                            "\n\t\t- Deluxe burger\n\t\t- Charm burger\n\tEnter your choise: ");
->>>>>>> bafdce6d0c03497b6dff2196546dcf5c16cf746e
+
                     String type = Main.scanner.nextLine();
                     Burger burger = Burger.singleBurgerOrder(type);
                     System.out.println("\t" + burger.burgerInitialization(type) + " in the " +
@@ -58,11 +55,8 @@ public class Main {
                     System.out.print("\tChoose your menu - available options are:\n\t1) Basic menu:\n\t\t-Hamburger (no toppings)\n\t\t-small drink" +
                             "\n\t\t-small side item\n\t2) Cheeseburger menu\n\t\t-Cheeseburger (toppings included)\n\t\t-drink (size by choice)" +
                             "\n\t\t-side item (size by choice)\n\t3) Charming menu\n\t\t-Charmburger (toppings included)\n\t\t-drink (size by choice)" +
-<<<<<<< HEAD
                             "\n\t\t-side item (size by choice)\n\tEnter your choice: [by entering the kind of burger menu]");
-=======
-                            "\n\t\t-side item (size by choice)\n\tEnter your choice: ");
->>>>>>> bafdce6d0c03497b6dff2196546dcf5c16cf746e
+
                     int menuChoice = scanner.nextInt();
                     scanner.nextLine();
                     String size;
@@ -93,11 +87,8 @@ public class Main {
                 case 0 -> quit = true;
             }
             System.out.print("\t__________________________________\n\t" +
-<<<<<<< HEAD
                     "Would you like anything else? [Y/N]-> ");
-=======
-                    "Would you like anything else? -> ");
->>>>>>> bafdce6d0c03497b6dff2196546dcf5c16cf746e
+
             String input = scanner.nextLine();
             System.out.println("\t__________________________________");
             if (input.toLowerCase().contains("n")) {
@@ -110,11 +101,8 @@ public class Main {
     }
 
     public static void availableOptions() {
-<<<<<<< HEAD
+
         System.out.print("\tAvailable options are [1-4]:\n\t\t" +
-=======
-        System.out.print("\tAvailable options are:\n\t\t" +
->>>>>>> bafdce6d0c03497b6dff2196546dcf5c16cf746e
                 "1) Single burger\n\t\t" +
                 "2) Single drink\n\t\t" +
                 "3) Single side item\n\t\t" +
